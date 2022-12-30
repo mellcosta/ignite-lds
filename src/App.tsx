91c2bcabd583c684ef0a2c21fb1a00a9d41d2d35
mellcosta <m3licosta@gmail.com>
2022-12-30 21:41:25 +0000

    :tada: First Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+      <div className='bg-black'>
+        <h1 className='p-5 text-violet-600'>Hello World</h1>
+      </div>
+  )
+}
